33b342b0f279129bb55365cbe7f8161431a07c49
Justin Lam <justin.t.lam@gmail.com>
2016-12-16 12:05:53 +0900

    Make sure to construct the calendar before anything uses it. Make sure to always use the calendar from the delegate instead of the current calendar.

--- CVCalendar Demo/CVCalendar Demo/ViewController.swift
+++ CVCalendar Demo/CVCalendar Demo/ViewController.swift
@@ -33,17 +33,19 @@ class ViewController: UIViewController {
     var selectedDay:DayView!
     
     var currentCalendar: Calendar?
-    
-    // MARK: - Life cycle
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+
+    override func awakeFromNib() {
         let timeZoneBias = 480 // (UTC+08:00)
         currentCalendar = Calendar.init(identifier: .gregorian)
         if let timeZone = TimeZone.init(secondsFromGMT: -timeZoneBias * 60) {
             currentCalendar?.timeZone = timeZone
         }
+    }
+    
+    // MARK: - Life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
         if let currentCalendar = currentCalendar {
             monthLabel.text = CVDate(date: Date(), calendar: currentCalendar).globalDescription
@@ -263,9 +265,9 @@ extension ViewController: CVCalendarViewDelegate, CVCalendarMenuViewDelegate {
     }
     
     
-    func disableScrollingBeforeDate() -> Date {
-        return Date()
-    }
+//    func disableScrollingBeforeDate() -> Date {
+//        return Date()
+//    }
     
     func maxSelectableRange() -> Int {
         return 14
