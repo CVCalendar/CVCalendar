cec49e5d104e23ef6e8afd837daabb3edf037ff9
Jonas-Taha El Sesiy <github@elsesiy.com>
2020-01-29 23:56:11 -0800

    Update project files for swift 5

--- CVCalendar Demo/CVCalendar Demo/ViewController.swift
+++ CVCalendar Demo/CVCalendar Demo/ViewController.swift
@@ -220,7 +220,7 @@ extension ViewController: CVCalendarViewDelegate, CVCalendarMenuViewDelegate {
     func supplementaryView(shouldDisplayOnDayView dayView: DayView) -> Bool {
         guard let currentCalendar = currentCalendar else { return false }
         
-        var components = Manager.componentsForDate(Foundation.Date(), calendar: currentCalendar)
+        let components = Manager.componentsForDate(Foundation.Date(), calendar: currentCalendar)
         
         /* For consistency, always show supplementaryView on the 3rd, 13th and 23rd of the current month/year.  This is to check that these expected calendar days are "circled". There was a bug that was circling the wrong dates. A fix was put in for #408 #411.
          
