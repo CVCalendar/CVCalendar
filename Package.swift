9e39cf6378454beb2b99e13a6a562a3c1ea3319b
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2020-02-18 22:54:16 -0800

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "CVCalendar",
+    platforms: [.iOS("8.0")],
+    products: [
+        .library(name: "CVCalendar", targets: ["CVCalendar"]),
+    ],
+    targets: [
+        .target(name: "CVCalendar", path: "CVCalendar")
+    ],
+    swiftLanguageVersions: [.v4_2]
+)
